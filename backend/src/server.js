dcc78570d46e7a9ec8eff01caf1f404c6236b746
masrafiio <masrafiislam505@gmail.com>
2025-10-24 01:17:10 +0600

    deployment work done

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,23 +3,39 @@ import todoRoutes from "./routes/todoRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(`Request received: Method: ${req.method}, Path: ${req.path}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`Request received: Method: ${req.method}, Path: ${req.path}`);
+//   next();
+// });
 
 app.use("/api/toDoList", todoRoutes);
 // here many many apis
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
